96e1f2605836145c5a8878e7b5a703af0954a71b
Nick Swoboda <nickswoboda@gmail.com>
2021-05-09 18:57:37 -0700

    Chore: Update Qt to 6.1.0 and build directory
    This update was needed in order to get the Core5Compat dll that QuaZip
    relies upon.
    
    Updated build directory to coincide with the build dir QtCreator.

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -1,17 +1,17 @@
 cd $(dirname "$0")
 cd ..
-if [ ! -d "build" ]; then 
-    mkdir build
+if [ ! -d "build/ExileHub-Desktop_Qt_6_1_0_MinGW_64_bit-Debug" ]; then 
+    mkdir -p build/ExileHub-Desktop_Qt_6_1_0_MinGW_64_bit-Debug
 fi
-cd build
+cd build/ExileHub-Desktop_Qt_6_1_0_MinGW_64_bit-Debug
 
 cmake -DCMAKE_BUILD_TYPE:STRING="Debug" \
 -DCMAKE_CXX_COMPILER:STRING="C:/Qt/Tools/mingw810_64/bin/g++.exe" \
 -DCMAKE_C_COMPILER:STRING="C:/Qt/Tools/mingw810_64/bin/gcc.exe" \
--DCMAKE_PREFIX_PATH:STRING="C:/Qt/6.0.3/mingw81_64" \
--DQT_QMAKE_EXECUTABLE:STRING="C:/Qt/6.0.3/mingw81_64/bin/qmake.exe" \
+-DCMAKE_PREFIX_PATH:STRING="C:/Qt/6.1.0/mingw81_64" \
+-DQT_QMAKE_EXECUTABLE:STRING="C:/Qt/6.1.0/mingw81_64/bin/qmake.exe" \
 -G"Ninja" \
-..
+../..
 
 Ninja
 
