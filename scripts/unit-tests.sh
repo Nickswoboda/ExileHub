4bd69a186c41f36866c5f7d14682f3080d558d90
Nick Swoboda <nickswoboda@gmail.com>
2021-05-30 17:04:42 -0700

    Chore: update unit-tests script
    Using the QtWebEngineWidgets requires the use of MSVC and qt5. However,
    running from MSVC requires that the x64 Native Tools environment
    variables are set. So in addition to updating the qt version to 5, I
    added a note explaining the previous statement. If this script is ran
    without the environment variables set properly, the cmake build will
    fail.

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -1,16 +1,22 @@
 cd $(dirname "$0")
 cd ..
-if [ ! -d "build/ExileHub-Desktop_Qt_6_1_0_MinGW_64_bit-Debug" ]; then 
-    mkdir -p build/ExileHub-Desktop_Qt_6_1_0_MinGW_64_bit-Debug
+if [ ! -d "build/ExileHub-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug" ]; then 
+    mkdir -p build/ExileHub-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug
 fi
-cd build/ExileHub-Desktop_Qt_6_1_0_MinGW_64_bit-Debug
+cd build/ExileHub-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug
 
-cmake -DCMAKE_BUILD_TYPE:STRING="Debug" \
--DCMAKE_CXX_COMPILER:STRING="C:/Qt/Tools/mingw810_64/bin/g++.exe" \
--DCMAKE_C_COMPILER:STRING="C:/Qt/Tools/mingw810_64/bin/gcc.exe" \
--DCMAKE_PREFIX_PATH:STRING="C:/Qt/6.1.0/mingw81_64" \
--DQT_QMAKE_EXECUTABLE:STRING="C:/Qt/6.1.0/mingw81_64/bin/qmake.exe" \
--G"Ninja" \
+#Because WebEngineWidgets isn't supported by mingw we have to use MSVC
+#Because we are using MSVC, this command needs to be run in either
+#the x64 Native Tools Command Prompt for VS2019
+#OR you must set up the environment variable in a different command prompt
+#using the vcvars64.bat file
+
+cmake -S C:/dev/ExileHub \
+-GNinja "-DCMAKE_BUILD_TYPE:String=Debug" \
+"-DQT_QMAKE_EXECUTABLE:STRING=C:/Qt/5.15.2/msvc2019_64/bin/qmake.exe" \
+"-DCMAKE_PREFIX_PATH:STRING=C:/Qt/5.15.2/msvc2019_64" \
+"-DCMAKE_C_COMPILER:STRING=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.28.29910/bin/HostX64/x64/cl.exe" \
+"-DCMAKE_CXX_COMPILER:STRING=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.28.29910/bin/HostX64/x64/cl.exe"
 ../..
 
 Ninja
