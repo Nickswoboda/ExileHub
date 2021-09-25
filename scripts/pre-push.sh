5f6b88b051cece1a461f186d748ce087c1b02353
Nick Swoboda <nickswoboda@gmail.com>
2021-09-25 16:43:00 -0700

    CI: add pre-push hook script
    This script is equivalent of what the old pre-commit script used to be.
    It formats files according to the clang.format, makes sure the source
    files pass the static analysis, and makes sure the application can build
    and successfully pass the unit tests.

---
+++ scripts/pre-push.sh
@@ -0,0 +1,33 @@
+#Pre-Push checks
+#Code should only be able to go into the repository when it is formatted 
+#correctly and passes both static analysis and unit testing
+
+FILES=$(git diff --cached --name-only | grep -E '.*\.(c|cpp|h|hpp)$')
+
+for FILE in $FILES
+do
+	echo Formatting $FILE
+	clang-format -i $FILE
+	git add $FILE
+done
+
+echo Formatting Complete
+
+FILES_SRC=$(git diff --cached --name-only | grep -E '.*\.(c|cpp)$')
+
+if [ -n "${FILES_SRC}" ]; then
+    echo Analyzing
+    cppcheck --error-exitcode=1 --enable=warning,style,performance $FILES_SRC;
+        if [ $? -eq 1 ]; then
+            echo Analysis Failed
+            exit 1
+        else
+            echo Analysis Complete
+        fi
+else
+    echo No files to analyze
+fi
+
+scripts/unit-tests.sh
+
+exit $?
