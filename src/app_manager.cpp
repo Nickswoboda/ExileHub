cafe312931319c7f9cff9afae8bb1e83a1e56ae8
Nick Swoboda <nickswoboda@gmail.com>
2021-09-30 23:36:50 -0700

    Feat(WIP): add GetLatestReleaseUrl func
    This func is used to translate a github.com/user/repo path to
    api.github.com/repos/user/repo/releases/latest. This url will be used to
    get download the latest release of the repository. Right now it only
    validates the path by seeing if it contains 'github.com'. In theory, a
    regex could be used to make sure it fits the expected input, but right
    now it will just fail the http request.

--- src/app_manager.cpp
+++ src/app_manager.cpp
@@ -4,10 +4,45 @@
 
 AppManager::AppManager(QObject* parent) : QObject(parent) {}
 
-bool AppManager::AddApp(bool from_github, const QString& path)
+bool AppManager::AddApp(bool from_github, QString& path)
 {
+  if (from_github) {
+    auto api_url = GetLatestReleaseUrl(path);
+    qDebug() << api_url;
+    // Check to see if github url is valid
+  }
   qDebug() << from_github;
   qDebug() << path;
 
   return true;
+}
+
+QString AppManager::GetLatestReleaseUrl(QString& path)
+{
+  // assuming url is http(s)://github.com/user/repo
+  // prepend 'api.' before github.com and append /repos/ after
+
+  static QString github_url = "github.com";
+
+  path = path.toLower();
+
+  int github_pos = path.indexOf(github_url);
+  if (github_pos == -1) {
+    return QString();
+  }
+
+  // In theory, could validate rest of path form with regex, but if ill-formed,
+  // the http request will fail anyways
+
+  path.insert(github_pos + github_url.size(), "/repos");
+  path.remove(0, github_pos);
+  path.prepend("https://api.");
+
+  if (!path.endsWith('/')) {
+    path.append('/');
+  }
+
+  path.append("releases/latest");
+
+  return path;
 }
--- src/app_manager.hpp
+++ src/app_manager.hpp
@@ -8,6 +8,10 @@ class AppManager : public QObject
 public:
   explicit AppManager(QObject* parent = nullptr);
 
-  bool AddApp(bool from_github, const QString& path);
+  bool AddApp(bool from_github, QString& path);
+
+private:
+  // Returns empty if invalid URL
+  QString GetLatestReleaseUrl(QString& path);
 signals:
 };
