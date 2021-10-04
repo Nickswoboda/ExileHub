a3097398c955cf3b0a855e33d0aab666e54f783a
Nick Swoboda <nickswoboda@gmail.com>
2021-10-03 22:29:03 -0700

    Feat(WIP): extract app name from executable path
    If no custom name is given to an App, the default name will be whatever
    the executable file is named

--- src/app.cpp
+++ src/app.cpp
@@ -1,6 +1,12 @@
 #include "app.hpp"
 
-App::App(const QString& executable_path) : executable_path_(executable_path) {}
+#include <QFileInfo>
+
+App::App(const QString& executable_path) : executable_path_(executable_path)
+{
+  QFileInfo info(executable_path_);
+  name_ = info.baseName();
+}
 
 App::App(const QString& name, const QString& executable_path)
     : name_(name), executable_path_(executable_path)
